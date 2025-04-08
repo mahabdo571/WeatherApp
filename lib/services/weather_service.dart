2124f0c799b687f92520b0458cdb3167a7c25326
mahmoud abdo <mah.abdo571@gmail.com>
2025-04-08 14:42:36 +0300

    Refactor WeatherService: extract base URL and API key into class properties

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -7,11 +7,13 @@ class WeatherService {
   final Dio dio;
 
   WeatherService(this.dio);
+  final String baseUrl = 'http://api.weatherapi.com/v1';
+  final String apiKey = 'e9f73241b8994800849122247230912';
 
   Future<WeatherModel> getCurrentWeather({required String city}) async {
     try {
       Response response = await dio.get(
-        'http://api.weatherapi.com/v1/forecast.json?key=e9f73241b8994800849122247230912&q=$city&days=1',
+        '$baseUrl/forecast.json?key=$apiKey&q=$city&days=1',
       );
 
       return WeatherModel.fromJson(response.data);
