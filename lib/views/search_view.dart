1c002aa217958ca92b181a3d3ed410e8290ad767
mahmoud abdo <mah.abdo571@gmail.com>
2025-04-08 13:45:00 +0300

    Enhance SearchView: add padding and input decoration to TextField

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -7,7 +7,27 @@ class SearchView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Search city')),
-      body: const Center(child: TextField()),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Center(
+          child: TextField(
+            onSubmitted: (value) {print(value);},
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: const BorderSide(color: Colors.green, width: 2),
+              ),
+              hintText: 'Enter city Name',
+              suffixIcon: const Icon(Icons.search),
+              labelText: 'Search',
+              contentPadding: const EdgeInsets.symmetric(
+                vertical: 22,
+                horizontal: 16,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
