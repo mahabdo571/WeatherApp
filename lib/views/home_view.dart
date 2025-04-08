7e7d05b3514eacf09cb20bae632056990f8a878c
mahmoud abdo <mah.abdo571@gmail.com>
2025-04-08 12:02:51 +0300

    Add search functionality: implement SearchView and integrate with HomeView

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app_setup/views/search_view.dart';
 import 'package:weather_app_setup/widgets/no_weather_body.dart';
 
 class HomeView extends StatelessWidget {
@@ -9,6 +10,20 @@ class HomeView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Weather App'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) {
+                    return const SearchView();
+                  },
+                ),
+              );
+            },
+            icon: Icon(Icons.search),
+          ),
+        ],
       ),
       body: const NoWeatherBody(),
     );
